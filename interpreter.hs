9e41b4360a31dfdf6c86cac5ec2af6e4798731f2
IFcoltransG <47414286+IFcoltransG@users.noreply.github.com>
2020-02-17 23:31:57 +1300

    Implement literal parsers

--- interpreter.hs
+++ interpreter.hs
@@ -1,1 +1,33 @@
-main = putStrLn "Not implemented"
+import qualified Data.Set as Set
+import qualified Data.Text as T
+
+digits = "0123456789_"
+
+data MESON = MESONInteger Integer
+           | MESONList [MESON]
+           | MESONString String
+           | MESONSet (Set.Set MESON)
+           | MESONTuple [MESON]
+           deriving (Eq, Ord)
+
+unescapeMap = [("%20"," "),
+               ("%0A","\n"),
+               ("%09","\t"),
+               ("%0B","\v"),--vertical tab
+               ("%25","%")] --note: this percent sign transformation must go last for obvious reasons
+
+onText f (a, b) = f (T.pack a) $ T.pack b
+
+unescape :: String -> String
+unescape xs = T.unpack . (\xs -> foldl (flip (onText T.replace)) xs unescapeMap) . T.pack $ xs
+
+--parseMESONCode :: String -> MESON
+--parseMESONCode f = head $ foldr parseMESONToken [] (words f)
+
+--parseMESONToken :: String -> [MESON] -> [MESON]
+--parseMESONToken str state | all (`elem` digits) str = (MESONInteger . read $ filter (/= '_') str) ++ state
+--parseMESONToken ('"':str) state = (unescape str) ++ state
+
+
+main = do
+     putStrLn "Not implemented"
