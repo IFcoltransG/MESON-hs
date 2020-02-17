8798c991f6542b29e33ec74ffc91d56a81be89c6
IFcoltransG <47414286+IFcoltransG@users.noreply.github.com>
2020-02-18 00:12:09 +1300

    Implement rest of parser

--- interpreter.hs
+++ interpreter.hs
@@ -8,7 +8,7 @@ data MESON = MESONInteger Integer
            | MESONString String
            | MESONSet (Set.Set MESON)
            | MESONTuple [MESON]
-           deriving (Eq, Ord)
+           deriving (Eq, Ord, Show)
 
 unescapeMap = [("%20"," "),
                ("%0A","\n"),
@@ -21,13 +21,18 @@ onText f (a, b) = f (T.pack a) $ T.pack b
 unescape :: String -> String
 unescape xs = T.unpack . (\xs -> foldl (flip (onText T.replace)) xs unescapeMap) . T.pack $ xs
 
---parseMESONCode :: String -> MESON
---parseMESONCode f = head $ foldr parseMESONToken [] (words f)
+parseMESONCode :: String -> MESON
+parseMESONCode f = head $ foldl parseMESONToken (error "Bottom of stack!") (words f)
 
---parseMESONToken :: String -> [MESON] -> [MESON]
---parseMESONToken str state | all (`elem` digits) str = (MESONInteger . read $ filter (/= '_') str) ++ state
---parseMESONToken ('"':str) state = (unescape str) ++ state
+parseMESONToken :: [MESON] -> String -> [MESON]
+parseMESONToken state str | all (`elem` digits) str           = (MESONInteger . read $ filter (/= '_') str) : state
+parseMESONToken state ('"':str)                               = (MESONString $ unescape str) : state
+parseMESONToken state ('(':xs) | (dropWhile (== ',') xs) == ")" = (MESONTuple $ take num state) : (drop num state)
+                where num = succ . length $ takeWhile (== ',') xs
+parseMESONToken ((MESONInteger num):state) "[]"               = (MESONList $ take numInt state) : (drop numInt state)
+                where numInt = fromIntegral num
+parseMESONToken ((MESONInteger num):state) "{}"               = (MESONSet . Set.fromList $ take numInt state) : (drop numInt state)
+                where numInt = fromIntegral num
 
 
-main = do
-     putStrLn "Not implemented"
+main = interact $ unlines . (map (show . parseMESONCode)) . lines
