ad24afb851e8856985df15f373b7790c615ac197
IFcoltransG <47414286+IFcoltransG@users.noreply.github.com>
2023-01-17 13:04:25 +1300

    Refactor and reformat

--- interpreter.hs
+++ interpreter.hs
@@ -3,36 +3,41 @@ import qualified Data.Text as T
 
 digits = "0123456789_"
 
-data MESON = MESONInteger Integer
-           | MESONList [MESON]
-           | MESONString String
-           | MESONSet (Set.Set MESON)
-           | MESONTuple [MESON]
-           deriving (Eq, Ord, Show)
+data MESON
+  = MESONInteger Integer
+  | MESONList [MESON]
+  | MESONString String
+  | MESONSet (Set.Set MESON)
+  | MESONTuple [MESON]
+  deriving (Eq, Ord, Show)
 
-unescapeMap = [("%20"," "),
-               ("%0A","\n"),
-               ("%09","\t"),
-               ("%0B","\v"),--vertical tab
-               ("%25","%")] --note: this percent sign transformation must go last for obvious reasons
-
-onText f (a, b) = f (T.pack a) $ T.pack b
+unescapeMap =
+  [ ("%20", " "),
+    ("%0A", "\n"),
+    ("%09", "\t"),
+    ("%0B", "\v"), --vertical tab
+    ("%25", "%") --note: this percent sign transformation must go last for obvious reasons
+  ]
 
 unescape :: String -> String
-unescape xs = T.unpack . (\xs -> foldl (flip (onText T.replace)) xs unescapeMap) . T.pack $ xs
+unescape = T.unpack . (\xs -> foldl (flip (onText T.replace)) xs unescapeMap) . T.pack
+  where
+    onText f (a, b) = f (T.pack a) $ T.pack b
 
 parseMESONCode :: String -> MESON
 parseMESONCode f = head $ foldl parseMESONToken (error "Bottom of stack!") (words f)
 
 parseMESONToken :: [MESON] -> String -> [MESON]
-parseMESONToken state str | all (`elem` digits) str           = (MESONInteger . read $ filter (/= '_') str) : state
-parseMESONToken state ('"':str)                               = (MESONString $ unescape str) : state
-parseMESONToken state ('(':xs) | (dropWhile (== ',') xs) == ")" = (MESONTuple $ take num state) : (drop num state)
-                where num = succ . length $ takeWhile (== ',') xs
-parseMESONToken ((MESONInteger num):state) "[]"               = (MESONList $ take numInt state) : (drop numInt state)
-                where numInt = fromIntegral num
-parseMESONToken ((MESONInteger num):state) "{}"               = (MESONSet . Set.fromList $ take numInt state) : (drop numInt state)
-                where numInt = fromIntegral num
+parseMESONToken state str | all (`elem` digits) str = (MESONInteger . read $ filter (/= '_') str) : state
+parseMESONToken state ('"' : str) = MESONString (unescape str) : state
+parseMESONToken state ('(' : xs) | dropWhile (== ',') xs == ")" = MESONTuple (take num state) : (drop num state)
+  where
+    num = succ . length $ takeWhile (== ',') xs
+parseMESONToken ((MESONInteger num) : state) "[]" = MESONList (take numInt state) : drop numInt state
+  where
+    numInt = fromIntegral num
+parseMESONToken ((MESONInteger num) : state) "{}" = (MESONSet . Set.fromList $ take numInt state) : drop numInt state
+  where
+    numInt = fromIntegral num
 
-
-main = interact $ unlines . (map (show . parseMESONCode)) . lines
+main = interact $ unlines . map (show . parseMESONCode) . lines
